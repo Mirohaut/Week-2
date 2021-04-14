e3f21d4c3d9dc9c5b975402c57e4155db3e7cbfd
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-04-14 13:55:48 +0300

    taas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// const catRouter = require('./routes/catRouter');
+const catRouter = require('./routes/catRouter');
 // const db = require('./database/db')
 // db.connect({
 //   host: process.env.DB_METROPOLIA,
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// app.use('/cat', catRouter);
+app.use('/cat', catRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
