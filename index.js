85e48c6011780e39858aef1b85d165d13533ea0a
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-04-14 13:31:29 +0300

    aq

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3000;
 const catRouter = require('./routes/catRouter');
 const userRouter = require('./routes/userRouter');
 
