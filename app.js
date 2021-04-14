4b5f2620881c6aa4bb8716849c607e11e88115a5
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-04-14 13:43:11 +0300

    uus

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const catRouter = require('./routes/catRouter');
-const db = require('./database/db')
-db.connect({
-  host: process.env.DB_METROPOLIA,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
+// const db = require('./database/db')
+// db.connect({
+//   host: process.env.DB_METROPOLIA,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS
+// })
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
