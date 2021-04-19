7b15b71e22d68fd80d139cb3af44fc62984fbd8c
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-04-19 21:35:42 +0300

    weekfive

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const catRouter = require('./routes/catRouter');
+const https = require('https');
+const fs = require('fs');
+const sslkey = fs.readFileSync('ssl-key.pem');
+const sslcert = fs.readFileSync('ssl-cert.pem')
+
 // const db = require('./database/db')
 // db.connect({
 //   host: process.env.DB_METROPOLIA,
@@ -12,13 +17,33 @@ const catRouter = require('./routes/catRouter');
 // })
 // app.use(express.static('public'));
 
+const options = {
+  key: sslkey,
+  cert: sslcert
+};
+
+const app = express();
+
+https.createServer(options, app).listen(8000);
+
+app.get('/', (req, res) => {
+res.send('Hello Secure World!');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.use('/cat', catRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+const http = require('http');
+
+http.createServer((req, res) => {
+      res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
+      res.end();
+}).listen(3000);
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// })
 
